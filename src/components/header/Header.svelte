18c2be185f65d30828c043a23f20fe68601b81de
Chris Woodward <Chris@evolvelab.io>
2021-09-10 17:14:06 -0700

    clean up the overall app code

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header class="flex justify-between w-full">
+	<div>
+		<img src={logo} alt="SvelteKit"/>
+	</div>
+
+	<nav>
+		<ul class="flex f-row space-x-3">
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+		</ul>
+	</nav>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+	import Header from '../components/header/Header.svelte';
+	import '../assets/tailwind.css';
 </script>
 
 <Header />
@@ -9,37 +9,6 @@
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+<footer class="absolute bottom-0 left-0 w-full">
+	<p class="flex mx-auto justify-center">This is a Test</p>
 </footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
